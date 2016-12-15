60f358e85f52e69fcab14ed594b348f640b66849
Sabir Alizada <sabir.alizadeh@gmail.com>
2016-12-16 00:21:56 +0100

    Cheking commit.

--- MarbleFun/src/Main.java
+++ MarbleFun/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public Main()
 	{
 		if(!glfwInit()){
-			System.out.println("GLFW failed to intialize");
+			System.out.println("GLFW failed to intialize..");
 			System.exit(1);
 		}
 		
