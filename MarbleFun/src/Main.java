6c30084a4edc9f0705befc627caf51a41b1c53f9
Sabir Alizada <sabir.alizadeh@gmail.com>
2016-12-16 00:13:38 +0100

    Project was created.

---
+++ MarbleFun/src/Main.java
@@ -0,0 +1,27 @@
+
+import static org.lwjgl.glfw.GLFW.*;
+
+public class Main {
+
+	public Main()
+	{
+		if(!glfwInit()){
+			System.out.println("GLFW failed to intialize");
+			System.exit(1);
+		}
+		
+		long win = glfwCreateWindow(640, 480, "Marble Fun", 0, 0);
+		
+		glfwShowWindow(win);
+		
+		while(!glfwWindowShouldClose(win)){
+			glfwPollEvents();
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Main();
+	}
+
+}
